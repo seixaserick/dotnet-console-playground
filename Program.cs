a0a041a714504fcee321e7377e4f75324f883a0c
Erick Vils <seixaserick77@gmail.com>
2022-06-06 12:52:56 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿
+
+class Playground
+{
+    public static void Main(string[] args)
+    {
+        RunFib();
+    }
+    public static void RunFib(){
+        TimeSpan duration;
+        DateTime start = DateTime.Now;
+        int n = 44, fib = 0;
+        fib = CalculateFibonacciTailRecursiveMethod(n);
+        duration = DateTime.Now - start;
+        Console.WriteLine($"Fib({n}) is {fib} and execution time is {duration.TotalMilliseconds} ms");
+        if (RunAgain())
+        {
+            RunFib();
+        }
+    }
+    public static int CalculateFibonacciTailRecursiveMethod(int n, int previous = 0, int current = 1)
+    {
+        if (n == 0) { return previous; }
+        if (n == 1) { return current; }
+        return CalculateFibonacciTailRecursiveMethod(n - 1, current, previous + current);
+    }
+    private static bool RunAgain() {
+        Console.WriteLine("Run again (Y/N)?");
+        var inputText = Console.ReadLine();
+        Console.Clear();
+        return inputText.ToUpper() == "Y";
+ 
+    }
+}
