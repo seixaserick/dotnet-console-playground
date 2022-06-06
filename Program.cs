b8bddf34249181fde6e2a8947c6bc678efa9727f
Erick Vils <seixaserick77@gmail.com>
2022-06-06 15:55:50 -0300

    Comparing Fibonacci Dynamic programming build with int[] array versus List<int> performance

--- Program.cs
+++ Program.cs
@@ -1,34 +1,73 @@
-﻿
+﻿using System.Diagnostics;
 
 class Playground
 {
     public static void Main(string[] args)
     {
-        RunFib();
+        RunFibs(4);
     }
-    public static void RunFib(){
+    public static void RunFibs(int n)
+    {
         TimeSpan duration;
-        DateTime start = DateTime.Now;
-        int n = 44, fib = 0;
-        fib = CalculateFibonacciTailRecursiveMethod(n);
+        int fibRec, fibDPA, fibDPL;
+        DateTime start;
+        Console.WriteLine("Starting Fibonacci calculations...");
+
+        start = DateTime.Now;
+        fibRec = CalculateFibonacciTailRecursive(n);
         duration = DateTime.Now - start;
-        Console.WriteLine($"Fib({n}) is {fib} and execution time is {duration.TotalMilliseconds} ms");
-        if (RunAgain())
-        {
-            RunFib();
-        }
+        Console.WriteLine($"FibRec({n}) is {fibRec} and execution time is {duration.TotalMilliseconds.ToString().PadLeft(10)} ms");
+
+        start = DateTime.Now;
+        fibDPA = CalculateFibonacciDynamicProgrammingArray(n);
+        duration = DateTime.Now - start;
+        Console.WriteLine($"FibDPA({n})  is {fibDPA} and execution time is {duration.TotalMilliseconds.ToString().PadLeft(10)} ms");
+
+        start = DateTime.Now;
+        fibDPL = CalculateFibonacciDynamicProgrammingList(n);
+        duration = DateTime.Now - start;
+        Console.WriteLine($"FibDPL({n})  is {fibDPL} and execution time is {duration.TotalMilliseconds.ToString().PadLeft(10)} ms");
+
+
+        if (RunAgain()) { RunFibs(n); }
     }
-    public static int CalculateFibonacciTailRecursiveMethod(int n, int previous = 0, int current = 1)
+    public static int CalculateFibonacciTailRecursive(int n, int previous = 0, int current = 1)
     {
         if (n == 0) { return previous; }
         if (n == 1) { return current; }
-        return CalculateFibonacciTailRecursiveMethod(n - 1, current, previous + current);
+        return CalculateFibonacciTailRecursive(n - 1, current, previous + current);
     }
-    private static bool RunAgain() {
+    public static int CalculateFibonacciDynamicProgrammingArray(int n)
+    {
+        int[] dp = new int[n + 1];
+        dp[0] = 0; dp[1] = 1;
+        if (n < 2) { return dp[n]; }
+        for (int i = 2; i <= n; i++)
+        {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+    public static int CalculateFibonacciDynamicProgrammingList(int n)
+    {
+        List<int> dp = new List<int>(n + 1);
+        dp.AddRange(new int[] { 0, 1 });
+        if (n < 2) { return dp[n]; }
+        for (int i = 2; i <= n; i++)
+        {
+            dp.Add(dp[i - 1] + dp[i - 2]);
+        }
+        return dp[n];
+    }
+    private static bool RunAgain()
+    {
         Console.WriteLine("Run again (Y/N)?");
         var inputText = Console.ReadLine();
-        Console.Clear();
-        return inputText.ToUpper() == "Y";
- 
+        if (inputText.ToUpper() != "Y")
+        {
+            Console.WriteLine("Bye bye...");
+            return false;
+        }
+        return true;
     }
 }
